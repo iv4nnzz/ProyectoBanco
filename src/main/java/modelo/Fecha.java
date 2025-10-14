9df504872ea552785e2e4a599feb6fff6b40ecc2
ivan <9spotzs@gmail.com>
2025-10-13 23:07:54 -0500

    Implementar clase Fecha para manejo de fechas

---
+++ src/main/java/modelo/Fecha.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author 9spot
+ */
+public class Fecha {
+    private int dia;
+    private int mes;
+    private int año;
+    
+    public Fecha(int dia, int mes, int año) {
+        this.dia = dia;
+        this.mes = mes;
+        this.año = año;
+    }
+    
+    public int getDia() {
+        return dia;
+    }
+    
+    public void setDia(int dia) {
+        this.dia = dia;
+    }
+    
+    public int getMes() {
+        return mes;
+    }
+    
+    public void setMes(int mes) {
+        this.mes = mes;
+    }
+    
+    public int getAño() {
+        return año;
+    }
+    
+    public void setAño(int año) {
+        this.año = año;
+    }
+    
+    @Override
+    public String toString() {
+        return dia + "/" + mes + "/" + año;
+    }
+}
