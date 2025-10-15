23f28192ae71dc3c6d15c76648643073c1a01cf6
ivan <9spotzs@gmail.com>
2025-10-14 22:07:14 -0500

    Integrar sistema completo en clase Principal

---
+++ src/main/java/principal/Principal.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package principal;
+
+/**
+ *
+ * @author 9spot
+ */
+import vista.Vista;
+
+public class Principal {
+    public static void main(String[] args) {
+        Vista vista = new Vista();
+        vista.mostrarMenuPrincipal();
+    }
+}
